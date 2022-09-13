1792e64f7e2e09f123c1f061c92db09cdb6481ab
SFN <service.sfn+bitbucket@gmail.com>
2022-09-13 08:00:18 +0200

    + Prefs Utility.

---
+++ app/src/main/java/de/fhswf/fahrradverleih/android/util/Prefs.kt
@@ -0,0 +1,12 @@
+package de.fhswf.fahrradverleih.android.util
+
+/**
+ * Utility-Klasse für persistente Key-Value Paare.
+ */
+object Prefs {
+    /**
+     * Key, unter welchem das Token für die Rad-Api abgelegt wird.
+     */
+    const val KEY_TOKEN = "rad_api_token"
+
+}
