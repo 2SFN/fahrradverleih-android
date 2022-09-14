d0c8df9132fe273591d2707e21901474e10d97d8
SFN <service.sfn+bitbucket@gmail.com>
2022-09-14 13:27:39 +0200

    ~ Model-Classes: Cleanup

--- app/src/main/java/de/fhswf/fahrradverleih/android/model/GeopositionT.kt
+++ app/src/main/java/de/fhswf/fahrradverleih/android/model/GeopositionT.kt
@@ -1,26 +1,8 @@
 package de.fhswf.fahrradverleih.android.model
 
 import kotlinx.serialization.Serializable
-import org.json.JSONObject
 
 @Serializable
 data class GeopositionT(
     val breite: Double = 0.0,
-    val laenge: Double = 0.0) {
-
-//    fun toJson(): JSONObject {
-//        return JSONObject()
-//            .put("breite", breite)
-//            .put("laenge", laenge)
-//    }
-//
-//    companion object {
-//        fun fromJson(obj: JSONObject): GeopositionT {
-//            return GeopositionT(
-//                breite = obj.optDouble("breite", 0.0),
-//                laenge = obj.optDouble("laenge", 0.0)
-//            )
-//        }
-//    }
-
-}
+    val laenge: Double = 0.0)
--- app/src/main/java/de/fhswf/fahrradverleih/android/model/Station.kt
+++ app/src/main/java/de/fhswf/fahrradverleih/android/model/Station.kt
@@ -1,32 +1,10 @@
 package de.fhswf.fahrradverleih.android.model
 
 import kotlinx.serialization.Serializable
-import org.json.JSONObject
 
 @Serializable
 data class Station(
     val id: String = "",
     val bezeichnung: String = "",
     val position: GeopositionT = GeopositionT(),
-    val verfuegbar: Int = 0) {
-
-//    fun toJson(): JSONObject {
-//        return JSONObject()
-//            .put("id", id)
-//            .put("bezeichnung", bezeichnung)
-//            .put("position", position.toJson())
-//            .put("verfuegbar", verfuegbar)
-//    }
-//
-//    companion object {
-//        fun fromJson(obj: JSONObject): Station {
-//            return Station(
-//                id = obj.optString("id"),
-//                bezeichnung = obj.optString("bezeichnung"),
-//                position = GeopositionT.fromJson(obj.getJSONObject("position")),
-//                verfuegbar = obj.optInt("verfuegbar")
-//            )
-//        }
-//    }
-
-}
+    val verfuegbar: Int = 0)
