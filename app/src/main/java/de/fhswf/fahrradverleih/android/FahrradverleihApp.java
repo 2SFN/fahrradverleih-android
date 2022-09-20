aca1120b67047c9308b88896b8a606f9fa12538e
SFN <service.sfn+bitbucket@gmail.com>
2022-09-20 08:46:58 +0200

    ~ Application-Class: Kotlin >> Java

---
+++ app/src/main/java/de/fhswf/fahrradverleih/android/FahrradverleihApp.java
@@ -0,0 +1,6 @@
+package de.fhswf.fahrradverleih.android;
+
+import android.app.Application;
+
+public class FahrradverleihApp extends Application {
+}
--- app/src/main/java/de/fhswf/fahrradverleih/android/FahrradverleihApp.kt
+++
@@ -1,7 +0,0 @@
-package de.fhswf.fahrradverleih.android
-
-import android.app.Application
-
-class FahrradverleihApp : Application() {
-
-}
